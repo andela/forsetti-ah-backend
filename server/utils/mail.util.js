ff5a6e80ae844638489cee5a1d9850bd85a744ce
Samson Abosede <samabos007@gmail.com>
2019-04-01 01:06:38 +0100

    164798905-story(emailNotification):implement email notification helper
    - write test for email helper
    - write helper method for sending email
    
    [Delivers #164798905]

---
+++ server/utils/mail.util.js
@@ -0,0 +1,49 @@
+import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+
+dotenv.config();
+
+const { EMAIL_ADDRESS, EMAIL_PASSWORD } = process.env;
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: EMAIL_ADDRESS,
+    pass: EMAIL_PASSWORD,
+  },
+});
+
+/**
+ * Send Email to recipient
+ * @param {Object} emailPayload - Information about Email to be sent
+ * @param {String} emailPayload.email - The Recipient Email
+ * @param {String} emailPayload.subject - The Email Subject
+ * @param {String} emailPayload.message - The message to be sent can include html tags
+ * @returns {Object} Response
+ */
+const sendMail = async (emailPayload) => {
+  const {
+    email,
+    subject,
+    message,
+  } = emailPayload;
+
+
+  const mailOptions = {
+    from: EMAIL_ADDRESS,
+    to: email,
+    subject,
+    html: message,
+  };
+  let response = {};
+  try {
+    response = await transporter.sendMail(mailOptions);
+    response.status = 'success';
+  } catch (error) {
+    response.status = 'failure';
+    response.message = error.message;
+  }
+  return response;
+};
+
+export default sendMail;
