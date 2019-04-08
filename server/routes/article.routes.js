d463981afc547de658528cb2b967cb39dde4381d
Samson Abosede <samabos007@gmail.com>
2019-04-08 10:45:04 +0100

    164798914-story(articleClap) User clap for article
    - implement clap functionality on articles
    - write test for the feature
    
    [Deliver #164798914]

--- server/routes/article.routes.js
+++ server/routes/article.routes.js
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-import { ArticleController, CommentController } from '../controllers';
+import { ArticleController, CommentController, ClapController } from '../controllers';
 import { tryCatch, createArticle, checkComments } from '../utils';
 import { signInAuth } from '../utils/users/permissions.util';
 
@@ -12,5 +12,7 @@ router.post('/', [signInAuth, createArticle], tryCatch(ArticleController.createA
 router.post('/:slug/comment', [checkComments, signInAuth], tryCatch(createComments));
 
 router.post('/:slug/comment/:commentid/thread', [checkComments, signInAuth], tryCatch(threadedComment));
+
+router.post('/:articleId/claps', signInAuth, tryCatch(ClapController.createClap));
 
 export default router;
